4856cc1c9a11b847b434e0cb33ec2e3e54ada955
yuta3003 <y.nakazawa3003+github@gmail.com>
2023-09-03 20:00:06 +0900

    :shower: modify dir

--- ec2_manager/main.py
+++ ec2_manager/main.py
@@ -1,7 +1,8 @@
-import boto3
 import os
 
-with open("reagion.txt", "r") as file:
+import boto3
+
+with open("reagion.txt", "r", encoding="utf-8") as file:
     regions = [line.strip() for line in file]
 
 
@@ -11,17 +12,17 @@ for region in regions:
     instances = []
     ec2 = boto3.client(
         "ec2",
-        aws_access_key_id = os.environ["AccessKey"],
-        aws_secret_access_key = os.environ["SecretAccessKey"],
-        region_name = region
+        aws_access_key_id=os.environ["AccessKey"],
+        aws_secret_access_key=os.environ["SecretAccessKey"],
+        region_name=region,
     )
 
     ec2_data = ec2.describe_instances()
-    for ec2_reservation in ec2_data['Reservations']:
-        for ec2_instance in ec2_reservation['Instances']:
+    for ec2_reservation in ec2_data["Reservations"]:
+        for ec2_instance in ec2_reservation["Instances"]:
             if ec2_instance["InstanceId"]:
                 instance = {}
-                instance["Status"] = ec2_instance['State']['Name']
+                instance["Status"] = ec2_instance["State"]["Name"]
                 instance["InstanceID"] = ec2_instance["InstanceId"]
                 instances.append(instance)
                 answer_dict["Region"] = region
@@ -29,4 +30,3 @@ for region in regions:
 
     if answer_dict:
         answer_list.append(answer_dict)
-print(answer_list)
